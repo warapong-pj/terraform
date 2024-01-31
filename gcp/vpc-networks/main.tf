7ab42530de57f142af82861599a60141aa58b90f
warapong-pj <warapong.pj@gmail.com>
2024-01-31 07:49:02 +0000

    add sample vpc-networks

---
+++ gcp/vpc-networks/main.tf
@@ -0,0 +1,35 @@
+provider "google" {}
+
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "9.0.0"
+
+
+  project_id   = "qwiklabs-gcp-04-f599890e8cf2"
+  network_name = "example-vpc"
+
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = "us-west1"
+    },
+    {
+      subnet_name           = "subnet-02"
+      subnet_ip             = "10.10.20.0/24"
+      subnet_region         = "us-west1"
+      subnet_private_access = "true"
+      subnet_flow_logs      = "true"
+      description           = "This subnet has a description"
+    },
+    {
+      subnet_name               = "subnet-03"
+      subnet_ip                 = "10.10.30.0/24"
+      subnet_region             = "us-west1"
+      subnet_flow_logs          = "true"
+      subnet_flow_logs_interval = "INTERVAL_10_MIN"
+      subnet_flow_logs_sampling = 0.7
+      subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+    }
+  ]
+}
