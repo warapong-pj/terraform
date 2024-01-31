642014a11f13ba6b569699c79c98e53debac99ad
warapong-pj <warapong.pj@gmail.com>
2024-01-31 15:46:12 +0700

    add sample gke

---
+++ gcp/gke/main.tf
@@ -0,0 +1,35 @@
+provider "google" {}
+
+module "gke" {
+  source  = "terraform-google-modules/kubernetes-engine/google"
+  version = "5.0.0"
+  
+  project_id                 = "qwiklabs-gcp-04-b6e5589f6e8a"
+  name                       = "gke-test-1"
+  region                     = "us-central1"
+  zones                      = ["us-central1-a", "us-central1-b", "us-central1-f"]
+  network                    = "vpc-01"
+  subnetwork                 = "us-central1-01"
+  ip_range_pods              = "us-central1-01-gke-01-pods"
+  ip_range_services          = "us-central1-01-gke-01-services"
+  http_load_balancing        = false
+  horizontal_pod_autoscaling = true
+  kubernetes_dashboard       = true
+  network_policy             = true
+
+  node_pools = [
+    {
+      name               = "default-node-pool"
+      machine_type       = "n1-standard-2"
+      min_count          = 1
+      max_count          = 100
+      disk_size_gb       = 100
+      disk_type          = "pd-standard"
+      image_type         = "COS"
+      auto_repair        = true
+      auto_upgrade       = true
+      preemptible        = false
+      initial_node_count = 80
+    },
+  ]
+}
