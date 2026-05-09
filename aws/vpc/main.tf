0b510c3b94100fe9bb2d00c769ac0067e42f7db8
warapong-pj <warapong-pj@gmail.com>
2026-05-09 12:34:29 +0700

    replace 2 tier architecture with 3 tier architecture

--- aws/vpc/main.tf
+++ aws/vpc/main.tf
@@ -9,25 +9,16 @@ module "vpc" {
   name = var.vpc_name
   cidr = var.cidr
 
-  # two-tier architecture
-  azs             = var.az
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
+  # three-tier architecture
+  azs              = var.az
+  public_subnets   = var.public_subnets
+  private_subnets  = var.private_subnets
+  database_subnets = var.database_subnets
 
+  # nat per az
   enable_nat_gateway     = true
-  single_nat_gateway     = true
-  one_nat_gateway_per_az = false
-
-  # # three-tier architecture
-  # azs              = var.az
-  # public_subnets   = var.public_subnets
-  # private_subnets  = var.private_subnets
-  # database_subnets = var.database_subnets
-
-  # # nat per az
-  # enable_nat_gateway     = true
-  # single_nat_gateway     = false
-  # one_nat_gateway_per_az = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
 
   map_public_ip_on_launch = true
 }
