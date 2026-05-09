a4630a692ae73fff79b326824d960b4409becd40
warapong-pj <warapong-pj@gmail.com>
2026-05-09 14:12:25 +0700

    add output policy name

--- aws/s3/outputs.tf
+++ aws/s3/outputs.tf
@@ -1,7 +1,7 @@
-output "s3_name" {
-  value = module.s3.s3_bucket_id
+output "s3_bucket_arn" {
+  value = module.s3_bucket.s3_bucket_arn
 }
 
-output "s3_arn" {
-  value = module.s3.s3_bucket_arn
+output "s3_policy" {
+  value = module.iam.name
 }
