6a3e8addcc6609a8b593798002c3abbf0a6b7b03
warapong-pj <warapong-pj@gmail.com>
2026-05-10 13:22:23 +0700

    add tags to resource

--- aws/s3/variables.tf
+++ aws/s3/variables.tf
@@ -2,6 +2,21 @@ variable "region" {
   default = "ap-southeast-1"
 }
 
+variable "access_key" {
+  default = ""
+}
+
+variable "secret_key" {
+  default = ""
+}
+
 variable "bucket_name" {
   default = "bucket-name"
+}
+
+variable "tags" {
+  default = {
+    "ManageByTerraform" : "True"
+    "Environment" : "POC"
+  }
 }
