ca651684cbf9b5677179a23e6897103366bfc38b
warapong-pj <warapong-pj@gmail.com>
2026-05-30 22:11:04 +0700

    update rds module

--- aws/rds/main.tf
+++ aws/rds/main.tf
@@ -1,12 +1,14 @@
 provider "aws" {
-  region = var.region
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+  version = "5.3.1"
 
-  name   = var.rds_name
+  name   = var.identifier
   vpc_id = var.vpc_id
 
   ingress_with_cidr_blocks = [
@@ -30,21 +32,36 @@ module "security_group" {
 
 module "rds" {
   source  = "terraform-aws-modules/rds/aws"
-  version = "6.3.0"
+  version = "7.2.0"
 
-  identifier = var.rds_name
+  identifier = var.identifier
 
   engine         = var.engine
   engine_version = var.engine_version
-  family         = var.family
-  instance_class = var.instance_class
 
-  allocated_storage = var.allocated_storage
+  apply_immediately = true
+  allow_major_version_upgrade = true
+
+  instance_class = var.instance_class
+  family         = var.family
 
   vpc_security_group_ids = [module.security_group.security_group_id]
 
-  publicly_accessible = false
-  manage_master_user_password = false
-  username            = var.rds_username
-  password            = var.rds_password
+  multi_az               = true
+  create_db_subnet_group = true
+  subnet_ids             = var.subnets_id
+
+  db_name                     = var.database
+  username                    = var.username
+  manage_master_user_password = true
+  password_wo                 = var.password
+  password_wo_version         = 1
+
+  allocated_storage = var.allocated_storage
+  max_allocated_storage = var.max_allocated_storage
+  kms_key_id        = "arn:aws:kms:ap-southeast-1:000000000000:key/dcb40258-7f41-4940-9067-1bef84b7f7fe"
+
+  deletion_protection = false
+
+  tags = var.tags
 }
